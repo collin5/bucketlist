66e372f2842b7ced0e0a122566e30d1930170d4b
Collins Abitekaniza <abtcolns@gmail.com>
2017-07-19 16:28:10 +0300

    Add get bucketlists functionality

--- api/bucketlist/tests/test_get_bucketlists.py
+++ api/bucketlist/tests/test_get_bucketlists.py
@@ -5,13 +5,14 @@
 # Date: 18.07.2017
 # Last Modified: 18.07.2017
 
-from api.app import app
+from api.app import app, db
 from unittest import TestCase, skip
 
 class GetBucketlistsTestCase(TestCase):
 
     def setUp(self):
         self.app = app.test_client()
+        db.create_all()
         # first register test user
         reg_form = {
                 "username": "collins",
@@ -26,17 +27,18 @@ class GetBucketlistsTestCase(TestCase):
         # Add bucket list
         first_bucket = {
                 "title": "First bucket",
-                "description": "lorem ipsum blah blah "
+                "description": "lorem ipsum blah blah ",
+                "token": self.token
                 }
         second_bucket = {
                 "title": "Second bucket",
-                "description": "lorem from second bucket is awesome"
+                "description": "lorem from second bucket is awesome",
+                "token": self.token
                 }
-        self.app.post('/auth/bucketlists', first_bucket)
-        self.app.post('/auth/bucketlists', second_bucket)
+        self.app.post('/bucketlists', data=first_bucket)
+        self.app.post('/bucketlists', data=second_bucket)
         
 
-    @skip
     def test_get_bucketlist_successfully(self):
         form = {
                 "token": self.token
@@ -44,7 +46,6 @@ class GetBucketlistsTestCase(TestCase):
         response = self.app.get('/bucketlists', data=form)
         self.assertEqual(response.status_code, 200)
 
-    @skip
     def test_get_bucketlists_successfully_content(self):
         response = self.app.get('/bucketlists', data={
             "token": self.token
@@ -53,12 +54,34 @@ class GetBucketlistsTestCase(TestCase):
         self.assertTrue("first bucket" in response.data.decode('utf-8').lower())
         self.assertTrue("second bucket" in response.data.decode('utf-8').lower())
 
-    @skip
+    def test_get_single_bucketlist_successfully(self):
+        form = {
+                "token": self.token
+                }
+        response = self.app.get('/bucketlists/1', data=form)
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_single_bucketlist_successfully_content(self):
+        form = {
+                "token": self.token
+                }
+        response = self.app.get('/bucketlists/2', data=form)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('second' in response.data.decode('utf-8').lower())
+
+    def test_get_bucketlist_out_of_range(self):
+        form = {
+                "token": self.token
+                }
+        response = self.app.get('/bucketlists/37', data=form)
+        self.assertEqual(response.status_code, 404)
+        self.assertTrue('no bucketlist found' in response.data.decode('utf-8').lower())
+
+
     def test_get_bucketlist_no_token(self):
         response = self.app.get('/bucketlists', data=None)
-        self.assertEqua(response.status_code, 403)
+        self.assertEqual(response.status_code, 403)
 
-    @skip
     def test_get_bucketlist_no_token_content(self):
         response = self.app.get('/bucketlists', data=None)
         self.assertEqual(response.status_code, 403)
