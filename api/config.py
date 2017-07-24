5c63abd88ba19850d2c7574ce7ac4ab6edb2b9ab
Collins Abitekaniza <abtcolns@gmail.com>
2017-07-24 17:59:10 +0300

    Add testcase for API pagination

--- api/config.py
+++ api/config.py
@@ -4,9 +4,11 @@
 # Author: Collins Abitekaniza <abtcolns@gmail.com>
 # Date: 10.07.2017
 # Last Modified: 10.07.2017
+import os
 
 
-SECRET_KEY = "\x84\xaeI\x13\n\x9fa  \n\xdf\x08\xdb'p\x024\x1a\x9d\xc7\x01N\x02\xb9"
+SECRET_KEY = os.getenv(
+    'SECRET_KEY') or "\x84\xaeI\x13\n\x9fa  \n\xdf\x08\xdb'p\x024\x1a\x9d\xc7\x01N\x02\xb9"
 
 SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format("sqlite.db")
 
