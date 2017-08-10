6e03d23e47d189d96b46bcd85bf70c4f60f8fbb7
Collins Abitekaniza <abtcolns@gmail.com>
2017-08-10 16:20:52 +0300

    Add login functionality

--- ngApp/src/app/components/login.component.ts
+++ ngApp/src/app/components/login.component.ts
@@ -8,17 +8,23 @@ import { AuthService } from '../services/auth.service';
 })
 export class LoginComponent {
     private error_msg: string = "";
-    constructor(private _auth: AuthService){
-
-    }
+    constructor(private _auth: AuthService){}
 
     onLogin(username, passphrase): void{
         this._auth.getToken(username, passphrase).subscribe(
-            (data: any) => this.authenticate,
-            error => this.error_msg = <any>error)
+            data => this.authenticate(data),
+            error => {
+                if (error.hasOwnProperty('error_msg')){
+                    this.error_msg = error['error_msg']
+                }
+            } 
+        )
     }
 
-    private authenticate(data){
-        console.log(data)
+    private authenticate(data: any): void{
+        if (data.hasOwnProperty('error_msg')){
+            this.error_msg = data['error_msg'];
+        }
     }
+
 }
--- ngApp/src/app/services/auth.service.ts
+++ ngApp/src/app/services/auth.service.ts
@@ -1,28 +1,35 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, URLSearchParams } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/do';
 
 @Injectable()
 export class AuthService{
     constructor( private _http: Http ){
 
     }
-    getToken(username: string, passphrase: string): Observable<any>{
+    getToken(username: string, passphrase: string): Observable<string>{
         let uri: string = "http://127.0.0.1:5000/auth/login";
-        return this._http.post(uri,{
-            username: username,
-            password: passphrase
-        }).map((response: Response) => <any>response.json()).catch(
+        let data = new URLSearchParams();
+        let obj={
+            "username": username,
+            "password": passphrase
+        };
+        for (let key in obj){
+            data.append(key, obj[key]);
+        }
+        return this._http.post(uri,data).map((response: Response) => <string>response.json())
+        .catch(
             this.errorHandler
         )
     }
 
     private errorHandler(error: Response){
         console.log(error);
-        return Observable.throw(error.json().error || 'Server Error')
+        return Observable.throw(error.json())
     }
 }
