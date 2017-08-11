f64ed17c9c3fa41faf23c024f86d8becfc889377
Collins Abitekaniza <abtcolns@gmail.com>
2017-08-11 17:25:28 +0300

    Add bucketlists component

--- ngApp/src/app/dashboard/dashboard.module.ts
+++ ngApp/src/app/dashboard/dashboard.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Dashboard } from './dashboard.component';
 import { SharedModule } from '../shared/shared.module';
+import { Bucketlists } from './bucketlists/bucketlist.component';
 
 @NgModule({
     declarations: [
-        Dashboard
+        Dashboard, Bucketlists
     ],
     imports: [
         SharedModule
