d496e4273f4c5b04fe0bb0ea2e61e4dfc0ed21ea
Collins Abitekaniza <abtcolns@gmail.com>
2017-08-20 03:54:44 +0300

    Add adding bucket functionality

--- ngApp/src/app/dashboard/bucketlists/bucketlist.component.ts
+++ ngApp/src/app/dashboard/bucketlists/bucketlist.component.ts
@@ -1,10 +1,42 @@
+declare var UIkit: any;
+
 import { Component } from '@angular/core';
+import { BucketlistService } from './bucketlist.service';
 
 @Component({
     selector: 'dash-bucketlists',
     moduleId: module.id,
-    templateUrl: 'bucketlists.component.html'
+    templateUrl: 'bucketlists.component.html',
+    providers: [ BucketlistService ]
 })
 export class Bucketlists{
-    title = 'My bucketlists';
+
+    constructor(private _bucketService: BucketlistService){}
+
+    title: string = 'My bucketlists';
+    add_bucket__err: string = null;
+
+    showAddSpinner = false;
+
+    onAdd(title, desc): void{
+        this.showAddSpinner = true;
+        this._bucketService.addBucket(title, desc).subscribe(
+            $data => {
+                if ($data.hasOwnProperty('error_msg')){
+                    UIkit.notification($data['error_msg'], {status: 'danger'});
+                }else{
+                    let $msg = ($data.hasOwnProperty('success_msg'))? $data['success_msg']: 'An error occured !';
+                    UIkit.modal('#add-bucket__modal').hide();
+                    UIkit.notification($msg, { status: 'success' });
+                }
+                this.showAddSpinner = false;
+            },
+            error => {
+                if (error.hasOwnProperty('error_msg')){
+                    this.add_bucket__err = error['error_msg']
+                }
+                this.showAddSpinner = false;
+            },
+        )
+    }
 }
